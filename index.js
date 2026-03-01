74deb1dec2554296fa65a19575dd03719cb4b274
takachi271-creator <takachi271@gmail.com>
2026-03-01 11:49:22 +0900

    add frontend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-app.use(express.static("public"));
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 let game = {
   players: {},
